0b89764662ef8ca84389a7d24ebf3e54560638bd
GregorySheets <gs489117@ohio.edu>
2019-03-25 17:23:17 -0400

    testing

--- randomNumber.cc
+++ randomNumber.cc
@@ -8,14 +8,48 @@
 using namespace std;
 
 //function prototypes
+void printArray(const int randomNumbers[], int size);
+
+int arraySearch(const int randomNumbers[], int search, int size);
 
 int main(int argc, char const *argv[]) {
-    int randomInteger;
     srand(time(NULL));
+    int randomInteger;
+    int randomNumbers[20];
+    int size = 20;
+    int search;
+    
     for(size_t i = 0; i < 20; i++)
     {
         randomInteger = rand();
-        cout << randomInteger << endl;
+        randomNumbers[i] = randomInteger;
+    }
+
+    printArray(randomNumbers, size);
+
+    cout << "Enter a target value: ";
+    cin >> search;
+
+    if(arraySearch(randomNumbers, search, size) >= 0) {
+        cout << "Value " << search << " is located at position " << (arraySearch(randomNumbers, search, size) + 1) << endl;
+    }
+    if(arraySearch(randomNumbers, search, size) == -1) {
+        cout << "Error: Number does not appear within the array." << endl;
     }
     return 0;
 }// main
+
+void printArray(const int randomNumbers[], int size) {
+    for(int i = 0; i < size; i++) {
+        cout << randomNumbers[i] << endl;
+    }
+}
+
+int arraySearch(const int randomNumbers[], int search, int size) {
+    for(int i = 0; i < size; i++) {
+        if(randomNumbers[i] == search) {
+            return i;
+        }
+    }
+    return -1;
+}
